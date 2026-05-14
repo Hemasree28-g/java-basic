9ebc4a3e42252de94d50eeb2c68e186036542914
hemasree <hemasree2827@gmail.com>
2026-05-14 22:53:41 +0530

    Add files via upload

---
+++ converting.java
@@ -0,0 +1,10 @@
+class converting
+{
+    public static void main(String args[])
+    {
+        int b = 234;
+        byte a=(byte)b;
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
