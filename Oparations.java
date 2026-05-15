536ef76009f791b6219bd535fbb2cc5ab8170d15
hemasree <hemasree2827@gmail.com>
2026-05-15 20:57:09 +0530

    Add files via upload

---
+++ Oparations.java
@@ -0,0 +1,15 @@
+class Oparations
+{
+    public static void main(String args[])
+    {
+        int a1=100+200;
+        int a2=300-400;
+        int a3=100*2;
+        int a4=200/10;
+       System.out.println(a1);
+       System.out.println(a2);
+       System.out.println(a3);
+       System.out.println(a4);
+    
+    }
+}
