f40cb7060a00bcac6c6b264bfd5358276c5d89c9
hemasree <hemasree2827@gmail.com>
2026-05-15 20:59:09 +0530

    Add files via upload

---
+++ IncDec.java
@@ -0,0 +1,14 @@
+class IncDec
+{
+    public static void main(String args[])
+    {
+        int a=5;
+       // a+=2;
+       //a-=2;
+       //a++;
+       //a--;
+       //--a;
+       a--=2;
+        System.out.println(a);
+    }
+}
