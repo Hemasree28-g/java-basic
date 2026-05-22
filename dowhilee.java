49551409872f216ade2da828f09c58f4c913d042
hemasree <hemasree2827@gmail.com>
2026-05-22 13:30:32 +0530

    Add files via upload

---
+++ dowhilee.java
@@ -0,0 +1,12 @@
+class dowhilee
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        do
+        {
+            System.out.println("omg"+i);
+            i++;
+        }while(i<=4);
+    }
+}
