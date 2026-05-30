3b341b0b73da966a0630e28996773ce3d683fbcd
hemasree <hemasree2827@gmail.com>
2026-05-30 09:54:42 +0530

    Add files via upload

---
+++ arrfor.java
@@ -0,0 +1,16 @@
+class arrfor
+{
+    public static void main(String args[])
+    {
+        int num[]=new int[4];
+        int i;
+         num[0]=2;
+         num[1]=4;
+         num[2]=6;
+         num[3]=8;
+        for(i=0;i<4;i++)
+        {
+            System.out.println(num[i]);
+        }
+    }
+}
