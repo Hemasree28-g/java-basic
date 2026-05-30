f1d64745133143888cc1395f9e516118cb8c781c
hemasree <hemasree2827@gmail.com>
2026-05-30 09:52:44 +0530

    Add files via upload

---
+++ arr2.java
@@ -0,0 +1,10 @@
+class arr2
+{
+    public static void main(String args[])
+    {
+        int num[]=new int[4];
+        num[2]=5;
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+    }
+}
